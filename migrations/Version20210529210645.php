98a46a035787cd6a5333bc612b0f53600f393f17
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2021-05-29 22:27:05 +0100

    Create Version20210529210645.php

---
+++ migrations/Version20210529210645.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210529210645 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, produit_id INT NOT NULL, parent_id INT DEFAULT NULL, content LONGTEXT NOT NULL, nickname VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updtaed_at DATETIME NOT NULL, INDEX IDX_5F9E962AF347EFB (produit_id), INDEX IDX_5F9E962A727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, price INT NOT NULL, path VARCHAR(255) NOT NULL, path2 VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, updtaed_at DATETIME NOT NULL, INDEX IDX_29A5EC2712469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rating (id INT AUTO_INCREMENT NOT NULL, produit_id INT NOT NULL, nicckname VARCHAR(255) NOT NULL, stars INT NOT NULL, INDEX IDX_D8892622F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AF347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A727ACA70 FOREIGN KEY (parent_id) REFERENCES comments (id)');
+        $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC2712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D8892622F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC2712469DE2');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A727ACA70');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AF347EFB');
+        $this->addSql('ALTER TABLE rating DROP FOREIGN KEY FK_D8892622F347EFB');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE rating');
+    }
+}
